8c9f660648857e59cfef37ce16deba866ecef1f2
Tanisha <jannatulnurtanisha63@gmail.com>
2026-03-11 18:51:22 +0600

    Added global storage plus load Issues spinner in my assignment

--- script.js
+++ script.js
@@ -15,4 +15,23 @@ function login() {
     }
 }
 
-// 
+// ------------------Global Storage-----------------
+
+let allIssues = []
+
+// -------------------Load issues-------------
+
+async function loadIssues(type="all") {
+    const container = document.getElementById("issuesContainer")
+
+    //spinner
+    container.innerHTML = `<div class = "col-span-4 flex justify-center py-10"><span class="loading loading-spinner loading-lg"></span></div>`
+
+    setActiveTab(type)
+
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+
+    const result = await res.json()
+
+}
+
