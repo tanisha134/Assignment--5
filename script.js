72ef94f0162fedff3f132326829520d2c87384db
Tanisha <jannatulnurtanisha63@gmail.com>
2026-03-11 21:15:55 +0600

    Added cards conditions and badge's to the assignment

--- script.js
+++ script.js
@@ -87,4 +87,51 @@ function displayIssues(type) {
     if(type === "close"){
         title.innerHTML = `Close Issues (${filtered.length})`
     }
+
+    // cards
+
+    filtered.forEach(issue=> {
+        const borderColor =
+        issue.status === "open"
+        ? "border-t-4 border-green-500"
+        : "border-t-4 border-purple-500"
+
+        container.innerHTML +=`<div onclick= "openModal(${issue.id}" class = "card bg-white shadow-md hover:shadow-xl transition cursor-pointer p-4 ${borderColor}">
+
+        <div class= "flex justify-between items-center mb-2">
+
+        <span class = "badge ${issue.priority === "high"? "bg-red-100 text-red-700": issue.priority === "medium"? "bg-yellow-100 text-yellow-600": "bg-gray-200 text-gray-700"}">
+        ${issue.priority}
+        </span>
+
+        </div>
+
+        <h2 class = "font-semibold text-md mb-1">${issue.title}</h2>
+
+        <p class = "text-sm text-gray-500 mb-3"> ${issue.description?.slice(0, 80)}</p>
+
+
+        <div class = "flex gap-2 mb-3">
+
+        ${issue.labels?.includes("bug")? `<span class = "badge bg-red-100 text-yellow-700">bug</span>`: ""}
+
+        ${issue.labels?.includes("help wanted")? `<span class= "badge bg-yellow-100 text-yellow-600">help wanted</span>` : ""}
+
+        ${issue.labels?.includes("enhancement")? `<span class= "badge bg-green-100 text-green-700"> enhancement</span>`: ""}
+
+        </div>
+
+        <hr class = "mb-2">
+
+        <div class = "flex justify-between text-xs text-gray-500">
+
+        <span>#${issue.id} ${issue.author}</span>
+
+        <span>#${issue.createdAt?.slice(0.10)}</span>
+
+        </div>
+
+        </div>`
+    })
 }
+
