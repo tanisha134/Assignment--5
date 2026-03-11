13d0fc6687116543a98fc1a7250e6c57ded50f2d
Tanisha <jannatulnurtanisha63@gmail.com>
2026-03-11 18:40:57 +0600

    Added login code in javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+// ------------------Login------------------
+
+function login() {
+
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    if(username === "admin" && password === "admin123") {
+
+        localStorage.setItem("loggedIn", true)
+        window.location.href = "dashboard.html"
+    }
+    else {
+        alert("Invalid Credentials")
+    }
+}
+
+// 
